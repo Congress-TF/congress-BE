497c54f8bdba1d618d48d6384645ba2dd12dfa39
안정후 <austinan@naver.com>
2024-01-22 00:38:07 +0900

    feat: init project

---
+++ src/main/java/com/congress/congressbe/CongressBeApplication.java
@@ -0,0 +1,13 @@
+package com.congress.congressbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongressBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CongressBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/congress/congressbe/CongressBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.congress.congressbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongressBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
