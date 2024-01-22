5a04e677cb09d1559d54f33f2e7e0f431ac74b48
안정후 <austinan@naver.com>
2024-01-22 11:50:10 +0900

    feat: core module settings

---
+++ core-module/src/test/java/com/congress/coremodule/CoreModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.congress.coremodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
