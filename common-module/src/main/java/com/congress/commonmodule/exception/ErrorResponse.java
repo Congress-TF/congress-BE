5d86e078631ca849fe3eecb3ea0b60dd8b540133
안정후 <austinan@naver.com>
2024-01-25 17:13:07 +0900

    feat: exception files & handling

---
+++ common-module/src/main/java/com/congress/commonmodule/exception/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.congress.commonmodule.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+    private final String message;
+    private final int errorCode;
+
+    private ErrorResponse(String message, int errorCode) {
+        this.message = message;
+        this.errorCode = errorCode;
+    }
+
+    public static ErrorResponse from(BusinessException exception){
+        return new ErrorResponse(exception.getMessage(), exception.getErrorCode());
+    }
+}
