bb5cfa1bd456b4dfb07d6d7b6d06104d566fc9db
안정후 <austinan@naver.com>
2024-01-25 17:14:52 +0900

    feat: for application running

--- api-module/src/main/java/com/congress/apimodule/ApiModuleApplication.java
+++ api-module/src/main/java/com/congress/apimodule/ApiModuleApplication.java
@@ -2,8 +2,10 @@ package com.congress.apimodule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.congress")
 public class ApiModuleApplication {
 
 	public static void main(String[] args) {
