299d4b4a2ba39b9c7e94c435a3e58e812563c0a4
안정후 <austinan@naver.com>
2024-01-25 17:14:38 +0900

    feat: member query service & repository

---
+++ core-module/src/main/java/com/congress/coremodule/member/domain/service/MemberQueryService.java
@@ -0,0 +1,31 @@
+package com.congress.coremodule.member.domain.service;
+
+import com.congress.commonmodule.exception.Error;
+import com.congress.coremodule.member.application.dto.MemberSignIn;
+import com.congress.coremodule.member.application.mapper.MemberMapper;
+import com.congress.coremodule.member.domain.entity.Member;
+import com.congress.coremodule.member.domain.exception.MemberException;
+import com.congress.coremodule.member.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MemberQueryService {
+
+    private final MemberRepository memberRepository;
+
+    public void signIn(MemberSignIn memberSignIn) {
+
+        Member member = MemberMapper.toMember(memberSignIn);
+        memberRepository.save(member);
+    }
+
+    public void signOut(Long memberId) {
+
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new MemberException(Error.MEMBER_NOT_FOUND));
+        member.deleteMember();
+    }
+}
